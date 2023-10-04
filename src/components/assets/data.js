ad2a007a8da7379e0ad1c91dc7ef623caa7fa818
Felix Ouma <moxiemeyoh@gmai.com>
2023-10-04 09:57:27 +0300

    data added

---
+++ src/components/assets/data.js
@@ -0,0 +1,32 @@
+import desktopHeroImage1 from './desktop-image-hero-1.svg';
+import desktopHeroImage2 from './desktop-image-hero-2.svg';
+import deskTopHeroImage3 from './desktop-image-hero-3.svg';
+
+import mobileHeroImage1 from './mobile-image-hero-1.svg';
+import mobimeHeroImage2 from './mobile-image-hero-2.svg';
+import mobileHeroImage3 from './mobile-image-hero-3.svg';
+
+import aboutDarkImage from './image-about-dark.svg';
+import aboutLightImage from './image-about-light.svg';
+
+const data = [
+    view1 = {
+        desktop: desktopHeroImage1,
+        mobile: mobileHeroImage1,
+        description: "We provide an unmatched quality, confort and styles for. Our experts combine form and function in bringing  your vision to life. Create a room in your own style with our collection and make your property a reflection of what you love."
+    },
+    view2 = {
+        desktop: deskTopHeroImage3,
+        mobile: mobimeHeroImage2,
+        description: "With Stores all over the world, it is easy for you to find furniture for your home or place of business. Locally, we are in most major cities throughout the country. Find our branch nearest you using our store locator. Any question ? Don't Hesitate to contact us today."
+    },
+    view3 = {
+        desktop: desktopHeroImage2,
+        mobile: mobileHeroImage3,
+        description: "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we undestand what customers want for their homes and offices."
+    },
+    about= {
+        light: aboutLightImage,
+        dark: aboutDarkImage,
+    }
+]
