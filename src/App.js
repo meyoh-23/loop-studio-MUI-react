2c94c38d022e0be6a9573d18a7f43f47be580f70
Felix Ouma <moxiemeyoh@gmai.com>
2023-10-04 10:10:33 +0300

    theming modificatin setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import { ThemeProvider, createTheme } from "@mui/material";
+
+// modified theming for the app
+const theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 576,
+      md: 768,
+      lg: 992,
+      xl: 1200,
+      xxl: 1400
+    }
+  }
+})
 
 function App() {
   return (
-    <div className="App">
-      Room landing page
-    </div>
-  );
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        Room landing page
+      </div>
+    </ThemeProvider>
+  )
 }
 
 export default App;
